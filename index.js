72f49031f1e22d586cba8da405fcedba17e0cdd0
zhaozhijie <zhaozj@raying.com>
2017-10-24 15:43:02 +0800

    mkdirs

--- index.js
+++ index.js
@@ -2,21 +2,25 @@ const book = require('./book')
 const _ = require('lodash')
 const tasks = require('./task.json')
 
-
-// format of task.json
-// {
-//   site:[
-//     ...bookConfigs
-//   ]
-// }
-// format of bookConfigs
-// {
-//   id: '',
-//   name: '',
-//   replaceMap: {
-//     src: tgt
-//   }
-// }
+/**
+ * @example
+ * // format of task.json
+ * {
+ *   site:[
+ *    ...bookConfigs
+ *    ]
+ *  }
+ *
+ * @example
+ * // format of bookConfigs
+ *  {
+ *    id: '',
+ *    name: '',
+ *    replaceMap: {
+ *      src: tgt
+ *    }
+ *  }
+ */
 _.each(tasks, (books, site) => {
   _.each(books, (bookConfig) => {
     new book[site](bookConfig).fetch()
